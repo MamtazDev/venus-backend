643d376c26890c9ed5e91ba48dd18e10f0496d33
mohi14 <mohiuddinkhan1409@gmail.com>
2024-02-08 10:52:29 +0600

    build command added

--- config/db.js
+++ config/db.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
       `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.6ds5s8q.mongodb.net/venus`
       // `mongodb://127.0.0.1:27017/venus`
     );
-    // console.log("Connected to the mongodb database successfully!");
     console.log("Connected to the mongodb database successfully!");
   } catch (error) {
     console.log("mongodb connection failed", error.message);
