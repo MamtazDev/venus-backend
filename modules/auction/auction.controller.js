7809382e4518b50095b3268b40813dfd37b9f50b
mohi14 <mohiuddinkhan1409@gmail.com>
2024-02-24 14:22:39 +0600

    bidding info fixing

--- modules/auction/auction.controller.js
+++ modules/auction/auction.controller.js
@@ -86,7 +86,8 @@ export const getAllAuctionsByLeagueId = async (req, res) => {
 };
 
 export const setTeamOwner = async (req, res) => {
-  const { owner, price } = req.body;
+  const { owner, price, leagueId } = req.body;
+  console.log(owner, "owner");
   try {
     const result = await Auction.findByIdAndUpdate(
       req.params.id,
@@ -99,10 +100,15 @@ export const setTeamOwner = async (req, res) => {
       select: "-createdAt -updatedAt -__v  -password",
     });
 
-    const info = await LeagueUser.updateOne(
-      { user: owner },
-      { $inc: { buyIn: price } }
+    const info = await LeagueUser.findOneAndUpdate(
+      {
+        user: owner,
+        league: leagueId,
+      },
+      { $inc: { buyIn: price } },
+      { new: true, useFindAndModify: false }
     );
+    console.log(info, "infooo");
     res.status(200).json({
       success: true,
       message: "Auction status changed Successfully!",
